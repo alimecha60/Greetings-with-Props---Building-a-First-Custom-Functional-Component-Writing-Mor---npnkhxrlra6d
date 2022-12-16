47c37e2be312e8c8f9510b1d2bb4e82b6a48e3fa
Ali <alimecha60@gamil.com>
2022-12-16 20:17:48 +0530

    changes done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,15 @@
 // write code for Welcome component here
 import React from "react";
+function Greeting (){
+    return(
+        <h2>Welcome to Newton School</h2>
+    );
+}
 function Welcome({name}){
     return (
        <>
        <h1>hey! {name}</h1>
-       <h2>Welcome to Newton School</h2>
+         <Greeting/>
        </>
     );
 }
